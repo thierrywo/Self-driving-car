54c9ea19345b7b9109a9c4432db4a5f2765674ca
109875 <109875@st-maartenscollege.nl>
2019-05-11 06:08:39 -0400

    sdc

--- Algemeen.js
+++ Algemeen.js
@@ -1,18 +1,15 @@
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+	document.getElementById("mySidebar").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+	document.getElementById("mySidebar").style.width = "0";
+	document.getElementById("main").style.marginLeft = "0";
 }
-
 $(document).ready(function() {
-
-		    $(".fa-search").click(function() {
-		       $(".search-box").toggle();
-		       $("input[type='text']").focus();
-		     });
-
- 		});
+	$(".fa-search").click(function() {
+		$(".search-box").toggle();
+		$("input[type='text']").focus();
+	});
+});
